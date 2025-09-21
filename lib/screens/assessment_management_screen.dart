ee7c5a7fdcea567ae66e4826632fb674e6f748f7
Ufuoma Gideon Iboyi <ufuomaiboyi23@yahoo.com>
2025-09-21 12:19:40 +0100

    Update assessment_management_screen.dart

--- lib/screens/assessment_management_screen.dart
+++ lib/screens/assessment_management_screen.dart
@@ -32,7 +32,7 @@ class _AssessmentManagementScreenState
     });
     try {
       final res =
-          await http.get(Uri.parse(https://attendancebackend-gjjw.onrender.com/assessmentslist'));
+          await http.get(Uri.parse(https://attendancebackend-gjjw.onrender.com/assessment/list'));
       if (res.statusCode == 200) {
         final data = jsonDecode(res.body) as List<dynamic>;
         setState(() {
